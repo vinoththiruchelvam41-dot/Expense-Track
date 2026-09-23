39bdf61119575171954d11acff0d1685cb2a7bab
vinoththiruchelvam41-dot <vinoththiruchelvam41@gmail.com>
2026-09-23 06:35:58 +0300

    Update ExpenseTrack lock fix

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='expense-track-admin-v1';
+const CACHE='expense-track-admin-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
